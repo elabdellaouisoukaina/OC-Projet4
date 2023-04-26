a9695c6da1ee73f3057f5ded016326e2279ae84c
Soukaïna El Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-04-26 02:42:34 +0200

    #2 - Validation champ e-mail + message d'erreur si mauvaise saisie

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -61,11 +61,26 @@ function validate(){
     document.getElementById("lastError").style.display = "block";
   }
 
+  // Validation champs e-mail et affichage du message d'erreur si saisie incorrecte
+  const email = document.getElementById("email").value;
+  const emailMatchPattern = email.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z0-9._%+-]/); // accepte les formats aa@aa.aa / aa.aa@aa.aa / etc
+  if (emailMatchPattern !== null) { // emailMatchPattern = null lorsque la saisie ne respecte pas le pattern
+    console.log(emailMatchPattern);
+    var emailValid = true;
+    document.getElementById("emailError").style.display = "none";
+  } else {
+    console.log(emailMatchPattern)
+    var emailValid = false;
+    document.getElementById("emailError").style.display = "block";
+  }
+
   // Vérifie que tous les champs sont valides et retourne un bouléen pour dire si entierté du form valide ou non
-  if (firstValid === true  && lastValid === true) { 
+  if (firstValid === true  && lastValid === true && emailValid === true) { 
     // Tous les champs sont vidés
     document.getElementById("first").value = "";
     document.getElementById("last").value = "";
+    document.getElementById("email").value = "";
+
     return true;
   } else {
     return false;
