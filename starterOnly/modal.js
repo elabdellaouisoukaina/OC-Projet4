22aefe1ba7a719fe092c784ce5a2f2ba528d4731
Soukaïna El Abdellaoui <elabdellaoui.soukaina@gmail.com>
2023-05-10 22:40:57 +0200

    #2 - Refactorisation du code : création de fonctions de validation dédiées

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -23,79 +23,115 @@ function launchModal() {
 
 
 
+//===============================================================================================================
+
+
+
 // Ferme la modale au click sur la X
 function closeModal() {
   modalbg.style.display = 'none';
 }
 
-// Valide le formulaire
-form.addEventListener("submit", (event)=>{
-  event.preventDefault(); // Evite rechargement de la page (comportement par défault)
-  if (validate() === true) {
-    // Affiche message de confirmation d'envoi du formulaire
-    document.getElementById("formSent").style.display = "block";
-  } else {
-    document.getElementById("formSent").style.display = "none";  
+// Valide le champs Prénom : retourne true si minimum 2 caractères / n'est pas vide
+function firstValid(first){ 
+  // Validation champs prénom et affichage du message d'erreur si saisie incorrecte
+  if (first.length >= 2) { // Saisie prénom minimum deux caractères, n'est pas vide
+    document.getElementById("firstError").style.display = "none";
+    return true;
+  } else { // Affichage du message d'erreur si saisie incorrecte
+    document.getElementById("firstError").style.display = "block";
+    return false;
   }
-})
+}
+
+// Valide le champs Nom : retourne true si minimum 2 caractères / n'est pas vide
+function lastValid(last){
+  if (last.length >= 2) {
+    document.getElementById("lastError").style.display = "none";
+    return true;
+  } else { // Affichage du message d'erreur si saisie incorrecte
+    document.getElementById("lastError").style.display = "block";
+    return false;
+
+  }
+}
+
+// Valide le champs E-mail : retourne true si suit le format aa@aa.aa ou aa.aa@aa.aa
+function emailValid(email){
+  const emailMatchPattern = email.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z0-9._%+-]/); // accepte les formats aa@aa.aa / aa.aa@aa.aa / etc
+
+  if (emailMatchPattern !== null) { // emailMatchPattern = null lorsque la saisie ne respecte pas le pattern
+    document.getElementById("emailError").style.display = "none";
+    return true;
+  } else { // Affichage du message d'erreur si saisie incorrecte
+    document.getElementById("emailError").style.display = "block";
+    return false;
+  }
+}
+
+// Valide le champs Date de naissance : retourne true si n'est pas vide et personne majeur de + de 18 ans (et > 120 ans)
+function birthdateValid(birthdate){
+  
+}
+
+// Valide le champs Nombre de tournois : retourne true si valeur numérique saisie > 100
+function nbContestValid(){
+  
+}
+
+// Valide le champs Quel tournois : retourne true si un bouton radio selectionné
+function whichContestValid(){
+  
+}
+
+// Valide le champs CGU : retourne true si doit être cochée
+function cguVaid(){
+  
+}
+
 
 // Vérifie la validité des champs saisis et renvoi true si formulaire correctement complété
-function validate(){
-  // Validation champs prénom et affichage du message d'erreur si saisie incorrecte
-  const first = document.getElementById("first").value;
-  if (first.length >= 2) { // Saisie prénom minimum deux caractères, n'est pas vide
-    var firstValid = true;
-    document.getElementById("firstError").style.display = "none";
-  } else {
-    var firstValid = false;
-    document.getElementById("firstError").style.display = "block";
-  }
+function validateAll(){
 
-  // Validation champs nom et affichage du message d'erreur si saisie incorrecte
-  const last = document.getElementById("last").value;
-  if (last.length >= 2) {
-    var lastValid = true;
-    document.getElementById("lastError").style.display = "none";
-  } else {
-    var lastValid = false;
-    document.getElementById("lastError").style.display = "block";
-  }
-
-  // Validation champs e-mail et affichage du message d'erreur si saisie incorrecte
-  const email = document.getElementById("email").value;
-  const emailMatchPattern = email.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z0-9._%+-]/); // accepte les formats aa@aa.aa / aa.aa@aa.aa / etc
-  if (emailMatchPattern !== null) { // emailMatchPattern = null lorsque la saisie ne respecte pas le pattern
-    var emailValid = true;
-    document.getElementById("emailError").style.display = "none";
-  } else {
-    var emailValid = false;
-    document.getElementById("emailError").style.display = "block";
-  }
-
-  // Validation champs date de naissance et affichage du message d'erreur si saisie incorrecte
-  const birthdate = document.getElementById("birthdate").value;
-  const birthdateMatchPattern = birthdate.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/); // Accepte JJ/MM/AAAA ou MM/DD/YYYY
-  if (birthdateMatchPattern !== null) { // Champs non vide
-    var birthdateValid = true;
-    document.getElementById("birthdateError").style.display = "none";
-  } else {
-    var birthdateValid = false;
-    document.getElementById("birthdateError").style.display = "block";
-  }
+  // // Validation champs date de naissance et affichage du message d'erreur si saisie incorrecte
+  // const birthdate = document.getElementById("birthdate").value;
+  // const birthdateMatchPattern = birthdate.match(/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/); // Accepte JJ/MM/AAAA ou MM/DD/YYYY
+  // if (birthdateMatchPattern !== null) { // Champs non vide
+  //   var birthdateValid = true;
+  //   document.getElementById("birthdateError").style.display = "none";
+  // } else {
+  //   var birthdateValid = false;
+  //   document.getElementById("birthdateError").style.display = "block";
+  // }
 
 
-
+  var first = document.getElementById("first").value;
+  var last = document.getElementById("last").value;
+  var email = document.getElementById("email").value;
 
   // Vérifie que tous les champs sont valides et retourne un bouléen pour dire si entierté du form valide ou non
-  if (firstValid === true  && lastValid === true && emailValid === true && birthdateValid === true) { 
+  if (firstValid(first) === true  && lastValid(last) === true && emailValid(email) === true) { 
     // Tous les champs sont vidés
-    document.getElementById("first").value = "";
-    document.getElementById("last").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("birthdateValid").value = "";
-
+    first = "";
+    last = "";
+    email = "";
+    // document.getElementById("birthdateValid").value = "";
     return true;
   } else {
     return false;
   }  
 }
+
+
+// Valide le formulaire
+form.addEventListener("submit", (event)=>{
+  event.preventDefault(); // Evite rechargement de la page (comportement par défault)
+  if (validateAll() === true) {
+    // Affiche message de confirmation d'envoi du formulaire
+    document.getElementById("formSent").style.display = "block";
+  } else {
+    // Supprime message de confirmation lorsqu'envoi d'un deuxième formulaire à la suite mais invalide
+    document.getElementById("formSent").style.display = "none";  
+  }
+})
+
